3897ba1f4d21808ba236b347a781aae45d7d2e9a
piaochung <61677941+piaochung@users.noreply.github.com>
2020-06-02 11:19:50 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/user")
+const config = require("./config/key")
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://park:qwer1234@boilerplate-w4dzx.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongDB connected...'))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Wrld!'))
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보를 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
