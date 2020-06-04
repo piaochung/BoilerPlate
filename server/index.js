b3b320e8d20355959a6b20cffb8d1b9a3861628a
piaochung <61677941+piaochung@users.noreply.github.com>
2020-06-04 09:52:03 +0900

    완성 코드

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const bodyParser = require('body-parser');
 const { User } = require("./models/user");
 const config = require("./config/key");
@@ -18,6 +17,10 @@ mongoose.connect(config.mongoURI, {
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello Wrld!'))
+
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요~")
+})
 
 app.post('/api/users/register', (req, res) => {
 
@@ -85,6 +88,6 @@ app.get('/api/users/logout', auth, (req, res) => {
         })
 })
 
-
+const port = 5000;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
